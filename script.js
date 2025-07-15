5f9c0f48e4a5830c6d2967fd942783be73617766
anais-csa <anais10catalina@gmail.com>
2025-07-14 22:30:53 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+document.querySelectorAll('.semestre h2').forEach(h2 => {
+  h2.addEventListener('click', () => {
+    const ul = h2.nextElementSibling;
+    ul.style.display = ul.style.display === 'none' ? 'block' : 'none';
+  });
+});
 
